bfa107d737d51d9d035f06f9b98a3d7034444a0d
MSebb23 <matteosabbatini23@gmail.com>
2021-05-03 16:41:51 +0200

    tester
    it works

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    //variabili
+    $email = $_GET["email"];
+    $nonna = "Angela";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php echo $nonna; ?>
+    </h1>
+</body>
+</html>
