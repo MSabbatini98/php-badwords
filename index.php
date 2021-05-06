6d8b8eae51c5343fe6f86fd88e476b32b6b93ef5
MSebb23 <matteosabbatini23@gmail.com>
2021-05-06 22:57:33 +0200

    PHP badwords

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<!-- PHP -->
+
 <?php
-    //variabili
-    $email = $_GET["email"];
-    $nonna = "Angela";
+
+// ? Creare una variabile con un paragrafo di testo.
+// ? Visualizzare a schermo il paragrafo con la relative lunghezza e sestituire la badword passata in GET con  ***
+
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+
+$paragraph = "Le mosche non riposano mai perché la merda è davvero tanta.";
+
+if ( !empty($_GET['badword'])) {
+    $bad_word = $_GET['badword'];
+    $paragraph = str_replace($bad_word, '***', $paragraph);
+ } 
 
 ?>
+
+
+<!-- HTML -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,11 +27,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP - BAD WORD</title>
 </head>
 <body>
-    <h1>
-        <?php echo $nonna; ?>
-    </h1>
+<p>
+    La parola censurata è : <strong> <?php echo $bad_word; ?> </strong>
+    <br> 
+    <?php echo $paragraph; ?> 
+    <br>
+    La lunghezza del paragrafo è di <?php echo strlen($paragraph); ?> caratteri
+</p>
+    
 </body>
 </html>
